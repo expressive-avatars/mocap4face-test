675fea0516b093101856ff416f3532005c00f0fa
mattrossman <rossman@gatech.edu>
2021-12-15 00:51:08 -0500

    Map blendshapes onto RPM avatar

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import { Suspense, useRef } from 'react'
-import { OrbitControls, TorusKnot } from '@react-three/drei'
+import { Environment, OrbitControls, useGLTF } from '@react-three/drei'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { useFacetracking } from './hooks'
+import * as THREE from 'three'
 import { Webcam } from './Webcam'
+import { facemoji2arkit } from './blendShapes'
 
 export default function App() {
   const videoRef = useRef()
   return (
     <>
-      <Canvas>
+      <Canvas camera={{ fov: 35, position: [0, 0, 3] }}>
         <color attach="background" args={['black']} />
         <OrbitControls />
-        <Thing />
         <Suspense fallback={null}>
-          <Facetracker videoRef={videoRef} />
+          <Environment preset="warehouse" background />
+          <Model videoRef={videoRef} />
+          <Environment preset="warehouse" background />
         </Suspense>
       </Canvas>
       <Webcam ref={videoRef} play style={{ position: 'fixed', zIndex: 1, top: 1, transform: 'scale(-1, 1)' }} width={300} />
@@ -21,17 +24,34 @@ export default function App() {
   )
 }
 
-function Thing() {
-  const ref = useRef()
-  useFrame(() => (ref.current.rotation.y += 0.01))
-  return (
-    <TorusKnot ref={ref} args={[1, 0.3, 128, 16]}>
-      <meshNormalMaterial />
-    </TorusKnot>
-  )
-}
-
 function Facetracker({ videoRef }) {
   useFacetracking(videoRef)
   return null
+}
+
+const avatarURL = 'https://d1a370nemizbjq.cloudfront.net/54a8ca1e-1759-4cf9-ab76-bec155d6c83c.glb'
+
+function Model({ videoRef }) {
+  const { scene, nodes } = useGLTF(avatarURL)
+  /** @type {THREE.Mesh} */
+  const face = nodes.Wolf3D_Head
+
+  /** @type {THREE.Mesh} */
+  const teeth = nodes.Wolf3D_Teeth
+
+  /** @type {THREE.Bone} */
+  const headBone = nodes.Head
+
+  window.face = face
+
+  useFacetracking(videoRef, (blendShapes) => {
+    window.blendShapes = blendShapes
+    for (let key in blendShapes) {
+      const i = face.morphTargetDictionary[facemoji2arkit[key]]
+      face.morphTargetInfluences[i] = blendShapes[key]
+      teeth.morphTargetInfluences[i] = blendShapes[key]
+    }
+  })
+
+  return <primitive object={scene} position={[0, -2.4, 0]} scale={4} />
 }
